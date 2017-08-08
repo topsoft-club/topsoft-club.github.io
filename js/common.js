17dc7a62a8a2a5f189b21789efd9adf21d6ce3b3
MaximMRX <GM.PBIC@yandex.ru>
2017-08-09 03:14:07 +0700

    Updates

--- js/common.js
+++ js/common.js
@@ -1,48 +1,47 @@
 window.onload = function(){
-	setTimeout(function() { $('.page-loader-wrapper').fadeOut(); }, 50);
+	setTimeout(function() { $('.page-loader-wrapper').fadeOut().remove(); }, 50);
 }
-
-$(function() {
-
-});
 
 $(document).ready( function(){
   $("img, a").on("dragstart", function(event) { event.preventDefault(); });
-  
-  
-  $(".menu-toggle").on("click", function(){
-	
-	if($("html body").hasClass("ls-closed")){
-		$("html body").removeClass('ls-closed');
-	}else{
-		$("html body").addClass('ls-closed');
-	}
 
-  });
-
+  $.Admin.leftSideBar.activate();
 });
 
 
 $.Admin = {};
 
 $.Admin.options = {
-	colors: {
-		red: '#F44336',
+	leftSidebar: {
+		hiddenSize: 640 // на каком разрешении скрывать меню
 	},
-	leftSideBar: {
-		scrollColor: 'rgba(0,0,0,0.5)',
-		scrollWidth: '4px',
-		scrollAlwaysVisible: true,
-		scrollBorderRadius: '0',
-		scrollRailBorderRadius: '0'
-	},
-	dropdownMenu: {
-		effectIn: 'fadeIn',
-		effectOut: 'fadeOut'
-	}
 }
 
 $.Admin.leftSideBar = {
+	activate: function() {
+		var configs = $.Admin.options.leftSidebar;
 
-	
+		$(".menu-toggle").on("click", function(){
+			if($("html body").hasClass("ls-closed")){
+				$("html body").removeClass('ls-closed');
+			}else{
+				$("html body").addClass('ls-closed');
+			}
+		});
+
+	$(window).resize(function() {
+		if(document.documentElement.clientWidth < configs.hiddenSize) {
+			$("html body").addClass('ls-closed');
+		}else{
+			$("html body").removeClass('ls-closed');
+		}
+	});
+
+
+	}
 }
+
+
+
+
+
